3bf25e319f461dcde1e51dbf9566d98f94e3f45b
unknown <uremail>
2023-08-22 22:05:27 +0530

    ARK

---
+++ function.js
@@ -0,0 +1,68 @@
+// let message="Hi";
+
+// function showMessages(){
+//     let message="Hello,I'm Danish"; //local variable
+//     alert(message);
+// }
+
+
+//---------------------------(global)----------------------//
+// var greeter="hey hi";  
+// var times=4;
+
+// if(times>3){
+//     var greeter="say hello";
+// }
+
+// function showMessages(){
+//     var greeter="say hello instead";
+//     console.log(greeter)
+// }
+
+// console.log(greeter)
+// showMessages();
+// console.log(greeter)
+
+
+//---------------------(outer variabe)---------------//
+// let userName="Danish";
+
+// function showMessages(){
+//     let message="hello" + userName;
+//     alert(message);
+// }
+
+// showMessages();
+
+
+//-----------------------(for instance)---------------//
+// let userName="Danish";
+
+// function showMessage(){
+//     userName="Altaf";
+//     let message='Hello, '+userName;
+//     alert(message);
+// }
+
+// alert(userName);
+// showMessage();
+// alert(userName);
+
+//new function//
+
+function onePlusAvg(x,y){
+    return 1+ (x+y)/2
+}
+
+let a=1;
+let b=2;
+let c=3;
+
+console.log("One Plus Average of a and b is", onePlusAvg(a,b))
+console.log("One Plus Average of b and c is", onePlusAvg(b,c))
+console.log("One Plus Average of a and c is", onePlusAvg(a,c))
+
+
+
+
+
