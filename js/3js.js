702e9be1fc78ea8010336eb0ccbfda62852b9103
unknown <uremail>
2023-08-29 23:21:40 +0530

    js done

--- js/3js.js
+++ js/3js.js
@@ -11,7 +11,7 @@
 // prototypal inheritance
 // this call apply bind
 // pure & impure fncs
-// closures
+// closures (No)
 
 //--------------------High order functions-----------//
 //Higher Order Functions are the functions which accept a  
@@ -135,3 +135,143 @@
 // ans.getter()   // Now the value is changed from 12 to 24 due to setter
 
 //----------------Prototype---------------------------//
+// Predefined functions (write all this in the console eg.length)
+// var obj = {name:"harsh"}
+// obj
+// obj.hasOwnProperty("name")
+
+//-------------Prototypal inheritance--------------//
+// sarthak and muskan his student
+// var human = {
+//     canFly: false,
+//     canTalk: true,
+//     canWalk: true,
+//     HashChangeEventemotions: true,
+//     hadFourLegs: false //last sentence me commam nahi aayega
+// }
+
+// var sheriyansStudent = {
+//     canMakeAmazingWebsite: true,
+//     canMakeAwesomeAnimation: true,
+//     canMakeWorldClassAwardWebsite: true,
+// }
+
+// sheriyansStudent.__proto__ = human; //write sheriyansStudent in Console
+
+//----------------This call apply bind (This keyword)------------//
+
+// global => window
+// function => window
+// method => obj (always refer to parent obj)
+
+//jab bhi kuchh likh rahe ho check karo kya usmein kahi bhi koi fuction, object ya 
+//fir scope bana hai {}
+//jab bhi koi cheej {} brackets ke andar nahi hot to hum use global scope kahte hai
+
+// var a=12; //global scope
+// function abcd(){
+//     var a =12; //not global scope 12 is in abcd local scope
+// }
+
+// Global scope mein this ki value window hoti h
+// console.log(this)
+
+// Function scope mein 
+// function abcd(){
+//     console.log(this)
+// }
+// abcd();
+
+// Method Scope main this ki value obj hoti h
+// ek function jo object ke andar ho, use method kahte hai
+// in any method, "this" keyword always refers to parent object
+
+// var obj={
+//     name: "Harsh",
+//     baatkaro: function(){ // this is called as method
+//         console.log(this)
+//     } 
+// }
+// obj.baatkaro();
+
+// Event Listener
+// var button = document.querySelector("button");
+// button.addEventListener("click", function(){
+//     console.log(this); // o/p button
+//     this.style.color="red"; // o/p because this is refering button  
+
+//     button.style.color="red"; // we can use button but this is imp in this keyword 
+// })
+
+//------------ Call Apply Bind --------------- bind is mostly used react// 
+// Agar tumhare paas koi function hai and koi object hai and tumhe 
+// function chalana hai aur by default jo this ki value window hai use windoe naa rakh kar
+// point karwana hai kisi object ki taraf
+
+// function abcd(){
+//     console.log(this);
+// }
+
+// var obj = {age:24}
+// abcd(); // o/p window
+// abcd.call() // by default it is window
+// abcd.call(obj) //.call (now this will become obj)
+
+// function abcd(){  
+//     console.log(this.age); // need to understand this program line ans in next comment
+// }
+
+// var obj = {age:24}
+// abcd.call(obj) // obj chala jaye this mei (this.age) obj ke value=24 so o/p 24
+
+
+// function abcd(val, val2, val3){
+//     console.log(this, val, val2, val3);
+// }
+
+// var obj = {age:24}
+
+// abcd.call(obj,1,2,3) 
+// abcd.apply(obj, [1,2,3]) // array will not go in the parameter
+
+// function abcd(){
+//     console.log(this);
+// }
+
+// var obj = {age: 24}
+
+// abcd.bind(obj); // no o/p because it is just binded
+
+// var bindedfnc = abcd.bind(obj);
+// bindedfnc();
+
+//---------------- pure & impure fncs ----------------//
+
+// 1) it should always return same output for same input
+// 2) it will never change/updat the value of a global variable.
+
+// function abcd(val){
+//     return Math.random()*val;
+
+// }
+
+// var ans1 = abcd(2);
+// var ans2 = abcd(2);
+
+function abcd(a,b){ // condition 1) pure function
+    return a*b;
+}
+
+var ans1 = abcd(1,2);
+var ans2 = abcd(1,2);
+
+var abcdef = 12;
+function abcd(a,b){ //  condition 2) impure function
+    abcdef = 24; 
+    return a*b;
+}
+
+var ans1 = abcd(1,2);
+var ans2 = abcd(1,2);
+
+
